6c4d991b0f9ddfe28f47d0b9e7259bb17e3b17c8
bs482 <bs482@pvsd.org>
2018-11-27 11:22:07 -0500

    added code

---
+++ src/com/company/Tester.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by bs482 on 11/27/18.
+ */
+public class Tester {
+    public Tester ()
+    {
+
+    }
+}
---
+++ src/com/company/monte.java
@@ -0,0 +1,31 @@
+package com.company;
+
+/**
+ * Created by bs482 on 11/27/18.
+ */
+public class monte {
+    public double height, width, radius;
+
+    public monte (double h, double k, double r)
+    {
+        height = h;
+        width = k;
+        radius = r;
+    }
+
+    public double nextDropx ()
+    {
+        return Math.random()*width;
+    }
+
+    public double nextDropy ()
+    {
+        return Math.random()*height;
+    }
+
+    public boolean isInCircle ()
+    {
+        if(x-hrig)
+    }
+
+}
